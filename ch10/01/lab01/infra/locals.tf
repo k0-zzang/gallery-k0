c26b581832550310f5a4af3850268119907acc8c
k0-zzang <hyuna9582@gmail.com>
2026-05-21 17:17:04 +0900

    test

--- ch10/01/lab01/infra/locals.tf
+++ ch10/01/lab01/infra/locals.tf
@@ -4,7 +4,7 @@ locals {
   namespace = "${local.org}-${local.project}"
 
   vpc = {
-    name = "main"
+    name = "main2"
 
     cidr_block           = "10.0.0.0/16"
     enable_dns_support   = true
