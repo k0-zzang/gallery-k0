085fcbac7a87c40ab2a2d15079aededaf0b6e05c
k0-zzang <hyuna9582@gmail.com>
2026-05-22 12:09:12 +0900

    feat:백엔드

--- ch10/01/lab01/infra/providers.tf
+++ ch10/01/lab01/infra/providers.tf
@@ -8,13 +8,7 @@ terraform {
     }
   }
 
-  backend "s3" {
-    bucket       = "tf-core-tfstate-k0"
-    key          = "10.02/lab01/infra/terraform.tfstate-k0"
-    region       = "ap-northeast-2"
-    encrypt      = true
-    use_lockfile = true
-  }
+  backend "s3" {}
 }
 
 provider "aws" {
